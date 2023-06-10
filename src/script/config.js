1a0cdb10c79c32f39ffaa0007429f5285d7401a5
Arifin-MRX <arifinmrx@gmail.com>
2023-06-11 00:14:17 +0700

    first commit

---
+++ src/script/config.js
@@ -0,0 +1,5 @@
+const baseUrl = 'https://api.themoviedb.org/3/';
+const posterImageUrl = 'https://image.tmdb.org/t/p/w500';
+const apiKey = '9e36bd0c0fda809b5b9d9a6685436b7f';
+
+export { baseUrl, posterImageUrl, apiKey };
